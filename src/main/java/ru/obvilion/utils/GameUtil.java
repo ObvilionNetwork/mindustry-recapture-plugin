d1c4a580d9fdb56e1fe7961a4298e12a0bcdb459
KeviTV <zxcvbnm20042005@gmail.com>
2021-05-20 16:56:40 +0300

    feat: update threads

--- src/main/java/ru/obvilion/utils/GameUtil.java
+++ src/main/java/ru/obvilion/utils/GameUtil.java
@@ -1,25 +1,40 @@
 package ru.obvilion.utils;
 
-import arc.struct.Seq;
+import arc.Events;
 import arc.util.Timer;
 import mindustry.Vars;
+import mindustry.game.EventType;
+import mindustry.game.Team;
 import mindustry.game.Teams;
-import mindustry.gen.Building;
+import mindustry.gen.Groups;
+import mindustry.gen.Player;
 
 public class GameUtil {
     private static Timer.Task task;
+    private static Timer.Task taskLogic;
 
     public static void init() {
         if (task != null) task.cancel();
+        if (taskLogic != null) taskLogic.cancel();
 
-        task = Timer.schedule(GameUtil::updateCores, 0, 1f);
+        task = Timer.schedule(GameUtil::updateUsers, 0, 0.1f);
+        taskLogic = Timer.schedule(GameUtil::updateLogic, 0, 1.4f);
     }
 
-    public static void updateCores() {
-        Seq<Teams.TeamData> teams = Vars.state.teams.present;
-        for (Teams.TeamData team : teams) {
-            for (Building core : team.cores) {
-                core.health = Float.POSITIVE_INFINITY;
+    public static void updateLogic() {
+        if (Vars.state.teams.active.size < 2) {
+            if (Vars.state.teams.active.size == 1) {
+                Events.fire(new EventType.GameOverEvent(Vars.state.teams.active.first().team));
+            } else {
+                Events.fire(new EventType.GameOverEvent(Team.crux));
+            }
+        }
+    }
+
+    public static void updateUsers() {
+        for (Player pl : Groups.player) {
+            if (pl.team() == Team.sharded || pl.team().cores().size == 0) {
+                TeamHelper.updatePlayerTeam(pl);
             }
         }
     }
