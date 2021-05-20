7e669ffe8cfda83a8d1993f24f22537ea1b68d9e
KeviTV <zxcvbnm20042005@gmail.com>
2021-05-20 16:56:16 +0300

    feat: set rules on game start

--- src/main/java/ru/obvilion/events/EventsHelper.java
+++ src/main/java/ru/obvilion/events/EventsHelper.java
@@ -1,6 +1,11 @@
 package ru.obvilion.events;
 
+import arc.Events;
 import arc.util.Timer;
+import mindustry.content.Blocks;
+import mindustry.game.EventType;
+
+import static mindustry.Vars.state;
 
 public class EventsHelper {
     private static Timer.Task task;
@@ -11,5 +16,15 @@ public class EventsHelper {
         }
 
         task = Timer.schedule(CoreCaptureEvent::update, 0, 0.1f);
+
+        Events.on(EventType.PlayEvent.class, e -> {
+            state.rules.canGameOver = false;
+            state.rules.modeName = "Recapture";
+            state.rules.pvp = false;
+
+            Blocks.coreFoundation.health = Integer.MAX_VALUE;
+            Blocks.coreNucleus.health = Integer.MAX_VALUE;
+            Blocks.coreShard.health = Integer.MAX_VALUE;
+        });
     }
 }
